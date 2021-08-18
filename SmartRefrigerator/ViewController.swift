9ca4b16a8bce2b53896c4a1014da7c5828eb2fa7
hk-bae <68215452+hk-bae@users.noreply.github.com>
2021-08-18 20:56:49 +0900

    Initial Commit

---
+++ SmartRefrigerator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmartRefrigerator
+//
+//  Created by 배한결 on 2021/08/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
